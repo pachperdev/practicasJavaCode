34138fc95a93b52d3e85d9fc5d61881c71882344
Pachper_Dev <pachperdev@gmail.com>
2021-07-23 00:18:31 -0500

    ejersicio #7 terminado al 100%

--- sesion1/src/ej7.java
+++ sesion1/src/ej7.java
@@ -12,36 +12,40 @@ public class ej7 {
         int numeroDeClientes;
         int  contProductos = 1;
         int contClientes  = 1;
+        float totalVendido = 0f;
+        int contClientesFactura;
 
         System.out.println("ingrese el numero de clientes: ");
         numeroDeClientes = Leer.nextInt();
 
         for (int i = 1; i <= numeroDeClientes; i++) {
-        
-            System.out.println("Cliente " + contClientes + ":");
-            System.out.println("ingrese la cantidad de tipo de producto");
+            System.out.println("cliente " + contClientes + ":");
+            contClientes = contClientes + 1;
+
+            System.out.println("ingrese la cantidad de tipo de productos: ");
             cantidadTipoProductos = Leer.nextInt();
 
             for (int j = 1; j <= cantidadTipoProductos; j++) {
-
-                System.out.println("ingrese la cantidad del producto " +  contProductos + ":");
+                System.out.println("ingrese la cantidad del producto " + contProductos + ":");
                 cantidadDelProducto = Leer.nextInt();
 
-                System.out.println("ingrese el valor del producto " +  contProductos + ":");
-                valorProducto = Leer.nextInt();
+                System.out.println("ingrese el valor del producto " + contProductos + ":");
+                valorProducto = Leer.nextFloat();
+                contProductos = contProductos + 1; 
 
-                totalFactura = totalFactura + (cantidadDelProducto * valorProducto);
-                contProductos =  contProductos + 1;
-
-                if (cantidadTipoProductos == contProductos) {
+                if (contProductos > cantidadTipoProductos) {
                     contProductos = 1;
                 }
+                totalFactura = totalFactura + (cantidadDelProducto * valorProducto);
+                totalVendido = totalVendido + (cantidadDelProducto * valorProducto);
             }
-            
-            contClientes = contClientes +1;
+            contClientesFactura = contClientes - 1;  
+            System.out.println("total factura cliente " + contClientesFactura + " es: " + totalFactura);
+            System.out.println("");
+            totalFactura = 0f;
         }
 
-        System.out.println("el total de la factura es: " + totalFactura);
+        System.out.println("el total vendido es: " + totalVendido);
         System.out.println("gracias por su compra :) ");
 
         Leer.close();
