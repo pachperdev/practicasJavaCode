4646baea7d021fddaa716c27d3abf82b744d5cc1
Pachper_Dev <pachperdev@gmail.com>
2021-07-23 21:54:40 -0500

    termine por completo el ejercicio #1 de la sesion 2

--- sesion2/src/ej1.java
+++ sesion2/src/ej1.java
@@ -20,10 +20,27 @@ public class ej1 {
 
             System.out.println("introduzca la cantidad minima requerida del codigo: " + listaCodigo[i]);
             listaCantidadMinima[i] = Leer.nextInt();
-
-            int may=-1, cmay=0, men=1000000, cmen=0;
-            
         }
+
+        int may=-1, cmay=0, men=1000000, cmen=0;
+        System.out.println("codigos de productos que se necesitan pedir:");
+
+        for (int i = 0; i < numeroDeProductos; i++) {
+            if (listaCantidadBodega[i] < listaCantidadMinima[i]) {
+                System.out.println("debe pedir al provedor productos del codigo: " + listaCodigo[i]);
+            }
+            if (listaCantidadBodega[i] < men) {
+                men = listaCantidadBodega[i];
+                cmen = listaCodigo[i];
+            }
+            if (listaCantidadBodega[i] > may) {
+                may = listaCantidadBodega[i];
+                cmay = listaCodigo[i];
+            }
+        }
+
+        System.out.println("el codigo con el mayor valor es: " + cmay);
+        System.out.println("el codigo con el menor valor es: " + cmen);
 
         Leer.close();
     }
