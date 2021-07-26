4a7129062c12eb6a75ad8d5eb5cd5b09575a970e
Pachper_Dev <pachperdev@gmail.com>
2021-07-26 11:36:09 -0500

    ejeriicio #1 de la sesion3 parcialmente terminado, falta sacar los promedios para las cantidades de cada producto en bodega y su respectivo código

---
+++ sesion3/src/s3ej1.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class s3ej1 {
+    public static void main(String[] args) throws Exception {
+        Scanner Leer = new Scanner(System.in);
+
+        String listaCodigo[];
+        int [][] tablaCantidadBodega = new int[100][100];
+        int [][] tablaCantidadMinima = new int[100][100];
+
+        //354 256 127
+        System.out.println("digite los codigos separados por espacios");
+        String linea = Leer.nextLine();
+        listaCodigo = linea.split(" ");
+
+        int productos = listaCodigo.length;
+        System.out.println("el tamaño de los productos es: " + productos);
+
+        //25 30 8 10;21 67 84 21;46 36 96 25;1 22 33 44
+        //25 30 8 10
+        //21 67 84 21
+        //46 36 96 25
+        //1 22 33 44
+        //matriz 3x4 
+        System.out.println("digite la tabla de las cantidades en bodega separando las filas por punto y coma y las cantidades por espacio: ");
+
+        linea = Leer.nextLine();
+        String[] filas = linea.split(";");
+        // filas = ["25 30 8 10";"21 67 84 21";"46 36 96 25";"1 22 33 44"]
+
+        int sedes = filas.length;
+
+        for (int i = 0; i < sedes; i++) {
+            String[] cantidades = filas[i].split(" ");
+            //cantidades = ["25", "30", "8", "10"]
+            for (int j = 0; j < productos; j++) {
+                String num = cantidades[j];
+                tablaCantidadBodega[i][j] = Integer.parseInt(num);
+            }
+        }
+        //digitar la cantidad minima
+
+        System.out.println("digite la tabla de las cantidades minimas requeridas separando las filas por punto y coma y las cantidades por espacio: ");
+        linea = Leer.nextLine();
+        filas = linea.split(";");
+        for (int i = 0; i < sedes; i++) {
+            String[] cantidades = filas[i].split(" ");
+            for (int j = 0; j < productos; j++) {
+                String num = cantidades[j];
+                tablaCantidadMinima[i][j] = Integer.parseInt(num);
+            }
+        }
+
+        for (int i = 0; i < sedes; i++) {
+            for (int j = 0; j < productos; j++) {
+                if (tablaCantidadBodega[i][j] < tablaCantidadMinima[i][j]) {
+                    System.out.println("se debe solicitar producto " + listaCodigo[j] + " en sede " + i);
+                }
+            }
+        }
+
+        Leer.close();
+    }
+}
